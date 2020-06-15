82b0ab8f358788a6c31c44da2b4ec847216e2416
jihoon <dkekd9@naver.com>
2020-06-15 18:09:10 +0900

    Weather updated

--- app/src/main/java/com/ar/yonseiunivview/ui/menu/WeatherFragment.java
+++ app/src/main/java/com/ar/yonseiunivview/ui/menu/WeatherFragment.java
@@ -1,9 +1,11 @@
 package com.ar.yonseiunivview.ui.menu;
 
+import android.location.Address;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,20 +15,83 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.ar.yonseiunivview.R;
 
-public class WeatherFragment extends Fragment {
-    private WeatherViewModel mvm;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class WeatherFragment extends Fragment {
+    public static String BaseUrl = "http://api.openweathermap.org/";
+    public static String AppId = "e70b2a211c8d5acc99c685bff4fab094";
+    public static String lat = "37";
+    public static String lon = "129";
+    private TextView weatherData;
+    private Button btn;
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState){
-        mvm = ViewModelProviders.of(this).get(WeatherViewModel.class);
+                             ViewGroup container, Bundle savedInstanceState) {
+
         View root = inflater.inflate(R.layout.fragment_weather, container, false);
-        final TextView tv = root.findViewById(R.id.menu_fragment);
-        mvm.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+       weatherData = root.findViewById(R.id.weather);
+        btn = root.findViewById(R.id.btn_weather);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(String s) {
-                tv.setText(s);
+            public void onClick(View v) {
+                getCurrentData();
             }
         });
+
         return root;
     }
+
+    void getCurrentData() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BaseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        WeatherService service = retrofit.create(WeatherService.class);
+        Call<WeatherResponse> call = service.getCurrentWeatherData(lat, lon, AppId);
+        call.enqueue(new Callback<WeatherResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<WeatherResponse> call, @NonNull Response<WeatherResponse> response) {
+                if (response.code() == 200) {
+                    WeatherResponse weatherResponse = response.body();
+                    assert weatherResponse != null;
+
+                    String stringBuilder = "Country: " +
+                            weatherResponse.sys.country +
+                            "\n" +
+                            "Temperature: " +
+                            weatherResponse.main.temp +
+                            "\n" +
+                            "Temperature(Min): " +
+                            weatherResponse.main.temp_min +
+                            "\n" +
+                            "Temperature(Max): " +
+                            weatherResponse.main.temp_max +
+                            "\n" +
+                            "Humidity: " +
+                            weatherResponse.main.humidity +
+                            "\n" +
+                            "Pressure: " +
+                            weatherResponse.main.pressure;
+
+                    weatherData.setText(stringBuilder);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<WeatherResponse> call, @NonNull Throwable t) {
+                weatherData.setText(t.getMessage());
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/ar/yonseiunivview/ui/menu/WeatherViewModel.java
+++ app/src/main/java/com/ar/yonseiunivview/ui/menu/WeatherViewModel.java
@@ -7,9 +7,12 @@ import androidx.lifecycle.ViewModel;
 
 public class WeatherViewModel extends ViewModel {
     private MutableLiveData<String> mText;
+    private MutableLiveData<String> CurPos;
     public WeatherViewModel(){
+
         mText = new MutableLiveData<>();
         mText.setValue("Weather Fragment");
+
     }
     public LiveData<String> getText(){return mText;}
 }
