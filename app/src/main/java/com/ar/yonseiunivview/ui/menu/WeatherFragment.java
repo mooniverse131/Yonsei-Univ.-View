0d7dd9cd747d3f6d9a789f57fde71c1e6fbd6fb5
jihoon <dkekd9@naver.com>
2020-06-17 04:19:47 +0900

    Weather updated v.1.2

--- app/src/main/java/com/ar/yonseiunivview/ui/menu/WeatherFragment.java
+++ app/src/main/java/com/ar/yonseiunivview/ui/menu/WeatherFragment.java
@@ -1,6 +1,10 @@
 package com.ar.yonseiunivview.ui.menu;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Address;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +19,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.ar.yonseiunivview.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -34,9 +39,10 @@ public class WeatherFragment extends Fragment {
     public static String AppId = "e70b2a211c8d5acc99c685bff4fab094";
     public static String lat = "37";
     public static String lon = "128";
+    public static String iconUrl;
     private TextView weatherData;
     private ImageView weatherIcon;
-    private Button btn;
+    private FloatingActionButton btn;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -44,7 +50,6 @@ public class WeatherFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_weather, container, false);
 
         weatherIcon = root.findViewById(R.id.icon);
-
         weatherData = root.findViewById(R.id.weather);
         btn = root.findViewById(R.id.btn_weather);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -52,6 +57,7 @@ public class WeatherFragment extends Fragment {
             public void onClick(View v) {
                 getCurrentData();
             }
+
         });
 
         return root;
@@ -70,17 +76,18 @@ public class WeatherFragment extends Fragment {
                 if (response.code() == 200) {
                     WeatherResponse weatherResponse = response.body();
                     assert weatherResponse != null;
+
+                    iconUrl = "http://openweathermap.org/img/w/" + weatherResponse.weather.get(0).icon + ".png";
                     double temp = Math.round((weatherResponse.main.temp - 273.15) * 10) / 10.0;
                     double temp_max = Math.round((weatherResponse.main.temp_max - 273.15) * 10) / 10.0;
                     double temp_min = Math.round((weatherResponse.main.temp_min - 273.15) * 10) / 10.0;
                     String stringBuilder = "지역: 원주시\n" +
                             "현재 기온 : " + temp + "℃\n" +
-                            "최고/최저 기온 : " + temp_max + "℃\n/" + temp_min + "℃\n" +
+                            "최고/최저 기온 : " + temp_max + "℃ / " + temp_min + "℃\n" +
                             "날씨 : " + transferWeather(weatherResponse.weather.get(0).description) + "\n" +
                             "습도 : " + weatherResponse.main.humidity + "%\n";
                     weatherData.setText(stringBuilder);
-
-//                  weatherIcon.setImageIcon(weatherResponse.weather.get(0).icon);
+                    new DownloadImageTask().execute(iconUrl);
                 }
             }
 
@@ -90,7 +97,29 @@ public class WeatherFragment extends Fragment {
             }
         });
     }
+    private Bitmap loadImageFromNetwork(String url){
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream((InputStream)new URL(url).getContent());
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        /** The system calls this to perform work in a worker thread and
+         * delivers it the parameters given to AsyncTask.execute() */
+        protected Bitmap doInBackground(String... urls) {
+            return loadImageFromNetwork(urls[0]);
+        }
+
+        /** The system calls this to perform work in the UI thread and delivers
+         * the result from doInBackground() */
+        protected void onPostExecute(Bitmap result) {
+            weatherIcon.setImageBitmap(result);
+        }
+    }
 
     private String transferWeather(String weather){
         weather = weather.toLowerCase();
